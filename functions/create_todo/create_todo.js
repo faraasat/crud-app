ca25f8f112ceb4a876713a4e6ad8fd7c314cb8b7
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-14 13:41:16 +0500

    Updated Function

---
+++ functions/create_todo/create_todo.js
@@ -0,0 +1,40 @@
+const faunadb = require("faunadb"),
+  q = faunadb.query
+require("dotenv").config({
+  path: `.env`,
+})
+const cuid = require("cuid")
+
+var adminClient = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET_KEY,
+})
+
+const handler = async (event, context) => {
+  try {
+    const data = JSON.parse(event.body)
+    const result = await adminClient.query(
+      q.Create(q.Collection("todoApp"), {
+        data: {
+          id: cuid(),
+          task: data.title,
+          starred: false,
+        },
+      })
+    )
+
+    console.log(result)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data: result.ref.id }),
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    }
+  }
+}
+
+module.exports = { handler }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout/layout.component"
 import SEO from "../components/seo"
+import "./index.styles.css"
 
 const IndexPage = () => {
   return (
