8eca42ebfdf843b4df29e3de8fe4830b5cab2c70
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-14 10:26:01 +0500

    Trimmed Some Code

---
+++ src/components/layout/layout.component.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import "./layout.styles.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <main>{children}</main>
+      <footer
+        style={{
+          marginTop: `2rem`,
+        }}
+      >
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </>
+  )
+}
+
+export default Layout
