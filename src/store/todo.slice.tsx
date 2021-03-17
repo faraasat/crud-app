379b90f318ba55ed7ddcd49c070421745956c353
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-17 20:23:23 +0500

    Introduced the Redux Store

---
+++ src/store/todo.slice.tsx
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+
+export const fetchTodos: any = createAsyncThunk(
+  "data/fetchTodos",
+  async (data, thunkAPI) => {
+    const response = await fetch("/.netlify/functions/get_todos")
+    return await response.json()
+  }
+)
+
+export const TodoSlice = createSlice({
+  name: "todoSlice",
+  initialState: { todos: [], updateId: "", todoLoading: false },
+  reducers: {
+    searchTodos: (state, action) => {},
+    refreshComponent: (state, action) => {},
+  },
+  extraReducers: {
+    [fetchTodos.fulfilled]: (state, action) => {
+      console.log(state, action)
+      state.todos = action.payload
+      state.todoLoading = false
+    },
+    [fetchTodos.reject]: (state, action) => {
+      console.log("fetchTodos Rejected")
+      state.todoLoading = false
+    },
+    [fetchTodos.pending]: (state, action) => {
+      console.log("fetchStoreData Pending")
+      state.todoLoading = true
+    },
+  },
+})
+
+export const { searchTodos, refreshComponent } = TodoSlice.actions
+export const selectTodoData = (state: any) => ({
+  todoData: state.todoSlice.todos,
+  updateId: state.todoSlice.updateId,
+  todoLoading: state.todoSlice.todoLoading,
+})
+export const TodoSliceReducer = TodoSlice.reducer
